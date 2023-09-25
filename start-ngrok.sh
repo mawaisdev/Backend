6c8cfcbae8c9086fad2aefee29a042b58b60ccaa
Muhammad Awais <m.awais2030@gmail.com>
2023-09-25 12:43:56 +0500

    Modifed Ngrok Domain

--- start-ngrok.sh
+++ start-ngrok.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gnome-terminal -- ngrok http 4000
+gnome-terminal -- ngrok http --domain=noted-aardvark-superb.ngrok-free.app 4000
 
 # Explicitly mention the full path to nodemon
-/home/kwanso/Documents/Backend/node_modules/.bin/nodemon -r ts-node/register /home/kwanso/Documents/Backend/src/server.ts
+nodemon -r ts-node/register ./src/server.ts
